3960d3ea2f30e01f3ae4cd5bbf72e76042ead585
Fabio Cataldo <fabio.cataldo.ext@intive.com>
2026-05-06 14:21:13 -0300

    feature: some imports fixed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-
-import { WeatherDashboardComponent } from './pages/weather/weather-dashboard.component';
+import { WeatherDashboardComponent } from './pages/weather-dashboard/weather-dashboard.component';
 
 @Component({
   selector: 'app-root',
